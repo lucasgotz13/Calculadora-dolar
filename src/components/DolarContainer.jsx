7ffb4e2547dcb988cdf149933d7cc5243f7c11b2
Lucas Gotz <tutigotz@gmail.com>
2023-05-05 23:36:46 -0300

    Funcionalidad agregada

--- src/components/DolarContainer.jsx
+++ src/components/DolarContainer.jsx
@@ -1,18 +1,50 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function DolarContainer() {
-    const [dolarValues, setDolarValue] = useState(null) 
+    const [dolarValues, setDolarValue] = useState([]);
+    const [dolarInput, setDolarInput] = useState(1);
+
+    const handleInputChange = (e) => {
+        setDolarInput(e.target.value);
+    };
 
     useEffect(() => {
         fetch("https://www.dolarsi.com/api/api.php?type=valoresprincipales")
-            .then(res => res.json())
-            .then(data => setDolarValue(data))
-    }, [])
+            .then((res) => res.json())
+            .then((data) => {
+                const filteredData = data.filter(
+                    (dolar) =>
+                        dolar.casa.venta !== "No Cotiza" &&
+                        dolar.casa.venta !== "0"
+                );
+                return setDolarValue(filteredData);
+            });
+    }, [dolarInput]);
 
     return (
-        <div>DolarContainer</div>
-                
-    )
+        <>
+            <label htmlFor="inputDolar">Dolares:</label>
+            <input
+                type="number"
+                defaultValue={1}
+                onChange={(e) => handleInputChange(e)}
+                onKeyDownCapture={(e) => handleInputChange(e)}
+            />
+            <br />
+            {dolarValues.map((dolar) => (
+                <div key={dolar.casa.nombre}>
+                    <h1>{dolar.casa.nombre}</h1>
+                    <p>
+                        {(
+                            parseFloat(dolar.casa.venta.replace(",", ".")) *
+                            dolarInput
+                        ).toFixed(2)}
+                    </p>
+                    <br />
+                </div>
+            ))}
+        </>
+    );
 }
 
-export default DolarContainer
+export default DolarContainer;
